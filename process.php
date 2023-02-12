d61982edb085bdcfc0392541c6aeefd498f35921
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-02-12 11:43:46 +0530

    connected with db and added user

--- process.php
+++ process.php
@@ -1,11 +1,24 @@
 <?php
 
-echo 'Processing...';
+// Connecting to DB
+$conn = mysqli_connect('localhost', 'root', '', 'ajax_test');
 
+echo 'Processing...';
 
 // Check for POST variables
 if(isset($_POST['name'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
     echo 'POST: Your name is '.$_POST['name'];
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+
+    if(mysqli_query($conn, $query)){
+        echo 'USER Added...';
+    }
+    else
+    {
+        echo 'ERROR: '.mysqli_error($conn);
+    }
 }
 
 
