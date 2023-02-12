e5255eff912e4a8295fff6293a2dc4ffde463abe
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-02-12 11:16:48 +0530

    post form with POST request and AJAX

--- process.php
+++ process.php
@@ -2,6 +2,14 @@
 
 echo 'Processing...';
 
+
+// Check for POST variables
+if(isset($_POST['name'])){
+    echo 'POST: Your name is '.$_POST['name'];
+}
+
+
+// Check for GET variables
 if(isset($_GET['name'])){
     echo 'GET: Your name is '.$_GET['name'];
 }
